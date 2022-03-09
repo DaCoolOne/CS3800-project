b65091ebc747c05795aac9ec95960e0e605c42cb
Scott Conner <GiantScus@gmail.com>
2022-03-09 10:22:27 -0600

    Need to figure out how interp accesses kernel

--- LispInterp.cpp
+++ LispInterp.cpp
@@ -1,12 +1,17 @@
 
 
 #include "LispInterp.h"
+#include <stdexcept>
 
 void LispInterp::stackPush(uint32_t newVal) {
-    
+    if(m_stackSize >= MAX_STACK_SIZE) throw std::range_error("STACK OVERFLOW");
+    m_stack[m_stackSize++] = newVal;
 }
 
 uint32_t LispInterp::stackPop() {
-    
+    if(m_stackSize <= 0) throw std::range_error("STACK UNDERFLOW");
+    return m_stack[--m_stackSize];
 }
+
+
 
--- LispInterp.h
+++ LispInterp.h
@@ -5,28 +5,25 @@
 
 const uint32_t MAX_STACK_SIZE = 255;
 
+class LISP_KERNEL {
+    
+};
+
 class LispInterp {
     char* m_mem;
     uint32_t m_currentChar;
     
     uint32_t m_maxBound;
 
-    uint32_t* m_stack;
-    uint32_t stackSize;
+    uint32_t m_stack[MAX_STACK_SIZE];
+    uint32_t m_stackSize;
 
     void stackPush(uint32_t newVal);
     uint32_t stackPop();
 
 public:
     LispInterp(char* mem, uint32_t memStart, uint32_t memEnd):
-        m_currentChar(memStart), m_maxBound(memEnd), m_mem(mem), stackSize(0),
-        m_stack(new uint32_t[MAX_STACK_SIZE]) {}
-    LispInterp(LispInterp& other) {
-        
-    }
-    ~LispInterp() {
-        delete[] m_stack;
-    }
+        m_currentChar(memStart), m_maxBound(memEnd), m_mem(mem), m_stackSize(0) {}
 };
 
 #endif
